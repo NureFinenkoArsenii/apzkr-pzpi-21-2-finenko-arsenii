291e530d4482f0c6242f20849e292b40c4de39a7
Arsenii Finenko <149533833+NureFinenkoArsenii@users.noreply.github.com>
2024-08-26 11:37:33 +0300

    Add files via upload

---
+++ apzkr-pzpi-21-2-finenko-arsenii/Task1-Server/SmartWarDrones/Repositories/DroneRepository.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using SmartWarDrones.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace SmartWarDrones.Repositories
+{
+    public class DroneRepository
+    {
+        private readonly IMongoCollection<Drone> collection;
+        public DroneRepository()
+        {
+            collection = new MongoClient("mongodb://localhost:27017")
+            .GetDatabase("swd_db")
+            .GetCollection<Drone>("drones");
+        }
+        public Drone Insert(Drone drone)
+        {
+            var existingUser = GetByDroneName(drone.DroneName);
+            if (existingUser != null)
+                throw new Exception("Drone with same name already exists");
+            drone.DroneId = Guid.NewGuid();
+            collection.InsertOne(drone);
+            return drone;
+        }
+        public IReadOnlyCollection<Drone> GetAll()
+        {
+            return collection
+            .Find(x => true)
+           .ToList();
+        }
+
+        public IReadOnlyCollection<Drone> GetAllByDroneType(string dronetype)
+        {
+            return collection.Find(x => x.DroneType == dronetype).ToList();
+        }
+        public Drone GetById(Guid droneid)
+        {
+            return collection
+            .Find(x => x.DroneId == droneid)
+            .FirstOrDefault();
+        }
+        public Drone GetByDroneName(string dronename)
+        {
+            return collection
+            .Find(x => x.DroneName == dronename)
+           .FirstOrDefault();
+        }
+        public Drone GetByDroneNameAndSafetyCode(string dronename,
+       string safetycode)
+        {
+            return collection
+            .Find(x => x.DroneName == dronename &&
+            x.SafetyCode == safetycode)
+            .FirstOrDefault();
+        }
+
+        public Drone GetByDroneNameAndDroneType(string dronename,
+string dronetype)
+        {
+            return collection
+            .Find(x => x.DroneName == dronename &&
+            x.DroneType == dronetype)
+            .FirstOrDefault();
+        }
+        public async void CreateIndexes()
+        {
+            await collection.Indexes
+            .CreateOneAsync(new
+           CreateIndexModel<Drone>(Builders<Drone>.IndexKeys.Ascending(_ =>
+           _.DroneId)))
+            .ConfigureAwait(false);
+            await collection.Indexes
+            .CreateOneAsync(new
+           CreateIndexModel<Drone>(Builders<Drone>.IndexKeys.Ascending(_ =>
+           _.DroneName)))
+            .ConfigureAwait(false);
+        }
+        public void Update(Drone drone)
+        {
+            collection.ReplaceOne(x => x.DroneId == drone.DroneId, drone);
+        }
+        public void DeleteByName(string droneName)
+        {
+            collection.DeleteOne(x => x.DroneName == droneName);
+        }
+    }
+}
---
+++ apzkr-pzpi-21-2-finenko-arsenii/Task1-Server/SmartWarDrones/Repositories/StatsRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using SmartWarDrones.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace SmartWarDrones.Repositories
+{
+    public class StatsRepository
+    {
+        private readonly IMongoCollection<Stats> collection;
+        public StatsRepository(IConfiguration configuration)
+        {
+            var connString =
+           configuration.GetConnectionString("MongoDBConnection");
+            collection = new MongoClient(connString)
+            .GetDatabase("swd_db")
+           .GetCollection<Stats>("stats");
+        }
+        public Stats Insert(Stats state)
+        {
+            state.Id = Guid.NewGuid();
+            collection.InsertOne(state);
+            return state;
+        }
+
+        public Stats GetById(Guid id)
+        {
+            return collection
+            .Find(x => x.Id == id)
+            .FirstOrDefault();
+        }
+        public IReadOnlyCollection<Stats> GetByDroneId(Guid
+       DroneId)
+        {
+            return collection
+            .Find(x => x.DroneId == DroneId)
+            .ToList();
+        }
+        public Stats GetByDroneIdAndStatsType(Guid droneId, string statsType)
+        {
+            return collection
+                .Find(x => x.DroneId == droneId && x.StatsType == statsType)
+                .FirstOrDefault();
+        }
+        public void Delete(Guid stateId)
+        {
+            collection.DeleteOne((x) => x.Id == stateId);
+        }
+        public async void CreateIndexes()
+        {
+            await collection.Indexes
+            .CreateOneAsync(new
+           CreateIndexModel<Stats>(Builders<Stats>.IndexKeys.Ascending(_ =>
+           _.Id)))
+            .ConfigureAwait(false);
+            await collection.Indexes
+            .CreateOneAsync(new
+           CreateIndexModel<Stats>(Builders<Stats>.IndexKeys.Ascending(_ =>
+           _.DroneId)))
+            .ConfigureAwait(false);
+        }
+        public void Update(Stats state)
+        {
+            collection.ReplaceOne(x => x.Id == state.Id, state);
+        }
+    }
+}
